d18971e62935bd3c5e9f9e626bf2b9ac28fb50ac
Pavel Snajdr <snajpa@snajpa.net>
2019-08-05 21:31:03 +0200

    [vpsAdminOS] syslog_ns: v3.1
    Changelog:
    
    v3.1:
      - Correctly virtualized log dumpers
      - Switch to new detect_syslog_namespace() function where possible
      - Port to Linux 5.6
    
    v3.0:
      - Based on syslog_ns series v2 by Rui Xiang
          https://lore.kernel.org/patchwork/cover/395692/
      - Hooked syslog namespace under user namespace
    
    Signed-off-by: Pavel Snajdr <snajpa@snajpa.net>
    Co-authored-by: Rui Xiang <rui.xiang@huawei.com>
    Co-authored-by: Petr Cvek <petrcvekcz@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1275,6 +1275,7 @@ struct task_struct {
 	/* Used by LSM modules for access restriction: */
 	void				*security;
 #endif
+	int				syslog_ns_for_child;
 
 #ifdef CONFIG_GCC_PLUGIN_STACKLEAK
 	unsigned long			lowest_stack;
