33a663077f314a6bd3f4147c833e5037a96dcb42
Pavel Snajdr <snajpa@snajpa.net>
2020-10-13 11:46:11 +0200

    [vpsAdminOS] sysinfo: virtualize the syscall
    Signed-off-by: Pavel Snajdr <snajpa@snajpa.net>

--- kernel/sys.c
+++ kernel/sys.c
@@ -2635,6 +2635,8 @@ static int do_sysinfo(struct sysinfo *info)
 	unsigned long mem_total, sav_total;
 	unsigned int mem_unit, bitcount;
 	struct timespec64 tp;
+	struct mem_cgroup *orig_memcg, *memcg;
+	unsigned long totalram = PAGE_COUNTER_MAX;
 
 	memset(info, 0, sizeof(struct sysinfo));
 
@@ -2672,6 +2674,43 @@ static int do_sysinfo(struct sysinfo *info)
 			goto out;
 	}
 
+	orig_memcg = get_mem_cgroup_from_mm(current->mm);
+	memcg = orig_memcg;
+	for (;;) {
+		unsigned long max;
+		if (!memcg)
+			break;
+		if (memcg == root_mem_cgroup)
+			break;
+		max = (u64)READ_ONCE(memcg->memory.max);
+		if (max < PAGE_COUNTER_MAX) {
+			totalram = max;
+			break;
+		}
+		if (parent_mem_cgroup(memcg) == root_mem_cgroup)
+			break;
+		memcg = parent_mem_cgroup(memcg);
+	}
+	if (totalram < PAGE_COUNTER_MAX) {
+		unsigned long memsw = PAGE_COUNTER_MAX;
+		unsigned long memsw_usage = 0;
+		unsigned long memusage = page_counter_read(&memcg->memory);
+
+		if (!cgroup_memory_noswap) {
+			memsw = READ_ONCE(memcg->memsw.max);
+			memsw_usage = page_counter_read(&memcg->memsw);
+		}
+
+		info->totalram = info->totalhigh = totalram;
+		info->freeram = info->freehigh = totalram - memusage;
+		if (memsw < PAGE_COUNTER_MAX) {
+			info->totalswap = memsw - totalram;
+			info->freeswap = info->totalswap - (memsw_usage - memusage);
+		}
+		info->bufferram = memcg_page_state(memcg, NR_FILE_PAGES) / PAGE_SIZE;
+		info->sharedram = memcg_page_state(memcg, NR_SHMEM) / PAGE_SIZE;
+	}
+	mem_cgroup_put(orig_memcg);
 	/*
 	 * If mem_total did not overflow, multiply all memory values by
 	 * info->mem_unit and set it to 1.  This leaves things compatible
