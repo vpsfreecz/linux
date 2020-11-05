450ac84a9b8f3a5a57a28057c2c428d3e88fd1f2
Pavel Snajdr <snajpa@snajpa.net>
2020-11-05 18:25:34 +0100

    [vpsAdminOS] syslog_ns: v4
    Changelog:
    v4
      - Port to printk ring buffer
    
    v3.2:
      - detect_syslog_namespace in vprintk_func()
    
    v3.1:
      - Correctly virtualized log dumpers
      - Switch to new detect_syslog_namespace() function where possible
      - Port to Linux 5.6
    
    v3.0:
      - Based on syslog_ns series v2 by Rui Xiang
          https://lore.kernel.org/patchwork/cover/395692/
      - Hooked syslog namespace under user namespace
    
    Signed-off-by: Pavel Snajdr <snajpa@snajpa.net>
    Co-authored-by: Rui Xiang <rui.xiang@huawei.com>
    Co-authored-by: Petr Cvek <petrcvekcz@gmail.com>
    Signed-off-by: Pavel Snajdr <snajpa@snajpa.net>

--- fs/proc/kmsg.c
+++ fs/proc/kmsg.c
@@ -14,36 +14,64 @@
 #include <linux/proc_fs.h>
 #include <linux/fs.h>
 #include <linux/syslog.h>
+#include <linux/user_namespace.h>
+#include <linux/syslog_namespace.h>
 
 #include <linux/uaccess.h>
 #include <asm/io.h>
 
-extern wait_queue_head_t log_wait;
-
 static int kmsg_open(struct inode * inode, struct file * file)
 {
-	return do_syslog(SYSLOG_ACTION_OPEN, NULL, 0, SYSLOG_FROM_PROC);
+	struct syslog_namespace *ns;
+
+	if (!current->nsproxy)
+		return -EFAULT;
+	ns = current->nsproxy->syslog_ns;
+
+	return do_syslog(SYSLOG_ACTION_OPEN, NULL, 0, SYSLOG_FROM_PROC, ns);
 }
 
 static int kmsg_release(struct inode * inode, struct file * file)
 {
-	(void) do_syslog(SYSLOG_ACTION_CLOSE, NULL, 0, SYSLOG_FROM_PROC);
+	struct syslog_namespace *ns;
+
+	if (!current->nsproxy)
+		return -EFAULT;
+
+	ns = current->nsproxy->syslog_ns;
+
+	(void) do_syslog(SYSLOG_ACTION_CLOSE, NULL, 0, SYSLOG_FROM_PROC, ns);
 	return 0;
 }
 
 static ssize_t kmsg_read(struct file *file, char __user *buf,
 			 size_t count, loff_t *ppos)
 {
+	struct syslog_namespace *ns;
+
+	if (!current->nsproxy)
+		return -EFAULT;
+
+	ns = current->nsproxy->syslog_ns;
+
 	if ((file->f_flags & O_NONBLOCK) &&
-	    !do_syslog(SYSLOG_ACTION_SIZE_UNREAD, NULL, 0, SYSLOG_FROM_PROC))
+	    !do_syslog(SYSLOG_ACTION_SIZE_UNREAD, NULL, 0, SYSLOG_FROM_PROC, ns))
 		return -EAGAIN;
-	return do_syslog(SYSLOG_ACTION_READ, buf, count, SYSLOG_FROM_PROC);
+	return do_syslog(SYSLOG_ACTION_READ, buf, count, SYSLOG_FROM_PROC,
+					current->nsproxy->syslog_ns);
 }
 
 static __poll_t kmsg_poll(struct file *file, poll_table *wait)
 {
-	poll_wait(file, &log_wait, wait);
-	if (do_syslog(SYSLOG_ACTION_SIZE_UNREAD, NULL, 0, SYSLOG_FROM_PROC))
+	struct syslog_namespace *ns;
+
+	if (!current->nsproxy)
+		return -EFAULT;
+
+	ns = current->nsproxy->syslog_ns;
+
+	poll_wait(file, &ns->log_wait, wait);
+	if (do_syslog(SYSLOG_ACTION_SIZE_UNREAD, NULL, 0, SYSLOG_FROM_PROC, ns))
 		return EPOLLIN | EPOLLRDNORM;
 	return 0;
 }
--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1310,6 +1310,7 @@ struct task_struct {
 	/* Used by LSM modules for access restriction: */
 	void				*security;
 #endif
+	int				syslog_ns_for_child;
 
 #ifdef CONFIG_GCC_PLUGIN_STACKLEAK
 	unsigned long			lowest_stack;
