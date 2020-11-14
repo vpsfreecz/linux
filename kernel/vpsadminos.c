ab14d18e9427df3ce6a998e49e1187e3ec988403
Pavel Snajdr <snajpa@snajpa.net>
2020-11-14 23:28:31 +0100

    [vpsAdminOS] Virtualize module parameters
    ... just fake them
    
    Signed-off-by: Pavel Snajdr <snajpa@snajpa.net>

--- kernel/vpsadminos.c
+++ kernel/vpsadminos.c
@@ -4,6 +4,8 @@
 #include <linux/vmstat.h>
 #include <linux/atomic.h>
 #include <linux/memcontrol.h>
+#include <linux/user_namespace.h>
+#include <linux/xarray.h>
 #include <asm/page.h>
 
 struct mem_cgroup *get_current_most_limited_memcg(void)
@@ -37,4 +39,83 @@ struct mem_cgroup *get_current_most_limited_memcg(void)
 not_found:
 	rcu_read_unlock();
 	return NULL;
+}
+
+struct fake_sysctl_buf {
+	ssize_t count;
+	char *buf;
+};
+
+void fake_sysctl_bufs_init(struct user_namespace *ns)
+{
+	xa_init(&ns->fake_sysctl_bufs);
+}
+
+void fake_sysctl_bufs_free(struct user_namespace *ns)
+{
+	unsigned long index;
+	struct fake_sysctl_buf *fbuf;
+	xa_for_each(&ns->fake_sysctl_bufs, index, fbuf) {
+		if (fbuf->buf)
+			kfree(fbuf->buf);
+		kfree(fbuf);
+	}
+}
+
+ssize_t fake_sysfs_kf_read(struct kernfs_open_file *of, char *buf)
+{
+	struct kobject *kobj = of->kn->parent->priv;
+	struct kobj_type *ktype = get_ktype(kobj);
+	unsigned long index = (unsigned long)of->kn;
+	struct user_namespace *ns = current_user_ns();
+	struct fake_sysctl_buf *fbuf;
+
+	if ((ktype == &module_ktype) && (ns != &init_user_ns)) {
+		fbuf = xa_find(&ns->fake_sysctl_bufs,
+		    &index, ULONG_MAX, XA_PRESENT);
+		if (fbuf) {
+			memcpy(buf, fbuf->buf, PAGE_SIZE);
+			pr_debug("%s:%d: kobj %lu fbuf at %p count %lu\n", __func__, __LINE__, index, (void *)fbuf, fbuf->count);
+			return fbuf->count;
+		}
+	}
+	return 0;
+}
+
+ssize_t fake_sysfs_kf_write(struct kernfs_open_file *of, char *buf,
+			    size_t count, loff_t pos)
+{
+	struct kobject *kobj = of->kn->parent->priv;
+	struct kobj_type *ktype = get_ktype(kobj);
+	unsigned long index = (unsigned long)of->kn;
+	struct user_namespace *ns = current_user_ns();
+	struct fake_sysctl_buf *fbuf;
+
+	if (!ns_capable(ns, CAP_SYS_ADMIN))
+		return -EPERM;
+
+	if ((count > PAGE_SIZE) || (pos > PAGE_SIZE))
+		return 0;
+
+	if ((ktype == &module_ktype) && (ns != &init_user_ns)) {
+		fbuf = xa_find(&ns->fake_sysctl_bufs,
+		    &index, ULONG_MAX, XA_PRESENT);
+		if (!fbuf) {
+			fbuf = kzalloc(sizeof(struct fake_sysctl_buf), GFP_KERNEL);
+			if (!fbuf)
+				return 0;
+			fbuf->buf = kzalloc(PAGE_SIZE, GFP_KERNEL);
+			if (!fbuf->buf) {
+				kfree(fbuf);
+				return 0;
+			}
+			xa_store(&ns->fake_sysctl_bufs,
+			    (unsigned long)of->kn, fbuf, GFP_KERNEL);
+		}
+		memcpy(fbuf->buf, buf, PAGE_SIZE);
+		fbuf->count = count;
+		pr_debug("%s:%d: kobj %lu, fbuf at %p, pos %llu, count %lu\n", __func__, __LINE__, index, (void *)fbuf, pos, count);
+		return count;
+	}
+	return 0;
 }
