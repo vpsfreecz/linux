b770c59b928acbdf59442940141c20fc135b602c
Pavel Snajdr <snajpa@snajpa.net>
2021-02-06 22:58:35 +0100

    [vpsAdminOS] syslog_ns: v6
    Changelog:
    v6
     - Port to 6.0
    
    v4.2
      - Use weaker locking for writes
    
    v4.1
      - Use kmalloc for syslog namespace buffers
    
    v4
      - Port to printk ring buffer
    
    v3.2:
      - detect_syslog_namespace in vprintk_func()
    
    v3.1:
      - Correctly virtualized log dumpers
      - Switch to new detect_syslog_namespace() function where possible
      - Port to Linux 5.6
    
    v3.0:
      - Based on syslog_ns series v2 by Rui Xiang <rui.xiang@huawei.com>
          https://lore.kernel.org/patchwork/cover/395692/
      - Hooked syslog namespace under user namespace
      - Contains fixes by Petr Cvek <petrcvekcz@gmail.com>
    
    Signed-off-by: Pavel Snajdr <snajpa@snajpa.net>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1468,6 +1468,7 @@ struct task_struct {
 	/* Used for BPF run context */
 	struct bpf_run_ctx		*bpf_ctx;
 #endif
+	int				syslog_ns_for_child;
 
 #ifdef CONFIG_GCC_PLUGIN_STACKLEAK
 	unsigned long			lowest_stack;
