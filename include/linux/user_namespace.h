8f7345b313c4f6dda452cf4a1dce0d181838acb7
Alexey Gladkov <gladkov.alexey@gmail.com>
2021-03-23 21:59:13 +0100

    Reimplement RLIMIT_NPROC on top of ucounts
    The rlimit counter is tied to uid in the user_namespace. This allows
    rlimit values to be specified in userns even if they are already
    globally exceeded by the user. However, the value of the previous
    user_namespaces cannot be exceeded.
    
    To illustrate the impact of rlimits, let's say there is a program that
    does not fork. Some service-A wants to run this program as user X in
    multiple containers. Since the program never fork the service wants to
    set RLIMIT_NPROC=1.
    
    service-A
     \- program (uid=1000, container1, rlimit_nproc=1)
     \- program (uid=1000, container2, rlimit_nproc=1)
    
    The service-A sets RLIMIT_NPROC=1 and runs the program in container1.
    When the service-A tries to run a program with RLIMIT_NPROC=1 in
    container2 it fails since user X already has one running process.
    
    We cannot use existing inc_ucounts / dec_ucounts because they do not
    allow us to exceed the maximum for the counter. Some rlimits can be
    overlimited by root or if the user has the appropriate capability.
    
    // snajpa edit:
    Based on v9 and fixed io uring wq

--- include/linux/user_namespace.h
+++ include/linux/user_namespace.h
@@ -50,8 +50,11 @@ enum ucount_type {
 	UCOUNT_INOTIFY_INSTANCES,
 	UCOUNT_INOTIFY_WATCHES,
 #endif
+	UCOUNT_RLIMIT_NPROC,
 	UCOUNT_COUNTS,
 };
+
+#define MAX_PER_NAMESPACE_UCOUNTS UCOUNT_RLIMIT_NPROC
 
 struct user_namespace {
 	struct uid_gid_map	uid_map;
@@ -110,6 +113,16 @@ void dec_ucount(struct ucounts *ucounts, enum ucount_type type);
 struct ucounts *alloc_ucounts(struct user_namespace *ns, kuid_t uid);
 struct ucounts * __must_check get_ucounts(struct ucounts *ucounts);
 void put_ucounts(struct ucounts *ucounts);
+
+static inline long get_ucounts_value(struct ucounts *ucounts, enum ucount_type type)
+{
+	return atomic_long_read(&ucounts->ucount[type]);
+}
+
+bool inc_rlimit_ucounts(struct ucounts *ucounts, enum ucount_type type, long v);
+bool inc_rlimit_ucounts_and_test(struct ucounts *ucounts, enum ucount_type type, long v, long max);
+void dec_rlimit_ucounts(struct ucounts *ucounts, enum ucount_type type, long v);
+bool is_ucounts_overlimit(struct ucounts *ucounts, enum ucount_type type, unsigned long max);
 
 #ifdef CONFIG_USER_NS
 
