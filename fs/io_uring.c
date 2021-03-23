7ca5cdb204f317e668b77a0bc94f585632a191de
Alexey Gladkov <gladkov.alexey@gmail.com>
2021-03-23 21:59:13 +0100

    Reimplement RLIMIT_NPROC on top of ucounts
    The rlimit counter is tied to uid in the user_namespace. This allows
    rlimit values to be specified in userns even if they are already
    globally exceeded by the user. However, the value of the previous
    user_namespaces cannot be exceeded.
    
    To illustrate the impact of rlimits, let's say there is a program that
    does not fork. Some service-A wants to run this program as user X in
    multiple containers. Since the program never fork the service wants to
    set RLIMIT_NPROC=1.
    
    service-A
     \- program (uid=1000, container1, rlimit_nproc=1)
     \- program (uid=1000, container2, rlimit_nproc=1)
    
    The service-A sets RLIMIT_NPROC=1 and runs the program in container1.
    When the service-A tries to run a program with RLIMIT_NPROC=1 in
    container2 it fails since user X already has one running process.
    
    We cannot use existing inc_ucounts / dec_ucounts because they do not
    allow us to exceed the maximum for the counter. Some rlimits can be
    overlimited by root or if the user has the appropriate capability.
    
    // snajpa edit:
    Based on v9 and fixed io uring wq

--- fs/io_uring.c
+++ fs/io_uring.c
@@ -7857,7 +7857,7 @@ static int io_init_wq_offload(struct io_ring_ctx *ctx,
 	unsigned int concurrency;
 	int ret = 0;
 
-	data.user = ctx->user;
+	data.cred = ctx->creds;
 	data.free_work = io_free_work;
 	data.do_work = io_wq_submit_work;
 
