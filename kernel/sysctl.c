3ac45ba06f352d21e39799cbc05e7aebddc5ff9d
Pavel Snajdr <snajpa@snajpa.net>
2022-09-25 22:39:42 +0200

    [vpsAdminOS] ksoftlimd v3: memcg soft_limit reclaim threads
    This patch introduces set of new 'ksoftlimd' kernel threads, which try to keep
    the soft_limit in check for select memory cgroup and its descendants by
    shrinking the LRU file backed inactive list.
    
    Direct reclaim path is modified to scan nearest memory cgroup running ksoftlimd
    and reclaim within its hieararchy.
    
    One thread is launched per every NUMA node present on the system. On direct
    reclaim, ksoftlimds are woken for nearest memory cgroup with ksoftlimds running.
    
    Single cgroup mode
    -------------------
    
    To launch ksoftlimd for a single memory cgroup and its descendants,
    
    echo 1 > <select_memory_cgroup>/memory.ksoftlimd_control
    
    To stop the ksoftlimd for the memory cgroup, echo 0 to the same file.
    
    Global cgroup_memory_ksoftlimd_for_all toggle
    ---------------------------------------------
    
    A new sysctl toggle is added to automatically launch ksoftlimd for all memory
    cgroups, which have a soft_limit setting.
    
    When we know that ksoftlimd is running for a cgroup in parent cgroup hierarchy,
    we can then skip running mem_cgroup_soft_limit_reclaim in kswapd path, which
    is both costly and unfair on machines, where one cgroup creates way more pages
    over soft_limit, than others on the same level.
    
    TODO: cgroup v2 fs support
    
    Signed-off-by: Pavel Snajdr <snajpa@snajpa.net>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -121,9 +121,7 @@ static unsigned long long_max = LONG_MAX;
 static int one_hundred = 100;
 static int two_hundred = 200;
 static int one_thousand = 1000;
-#ifdef CONFIG_PRINTK
 static int ten_thousand = 10000;
-#endif
 #ifdef CONFIG_PERF_EVENTS
 static int six_hundred_forty_kb = 640 * 1024;
 #endif
@@ -3219,6 +3217,35 @@ static struct ctl_table vm_table[] = {
 		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= SYSCTL_ZERO,
 		.extra2		= SYSCTL_ONE,
+	},
+#endif
+#ifdef CONFIG_MEMCG
+	{
+		.procname	= "cgroup_memory_ksoftlimd_for_all",
+		.data		= &cgroup_memory_ksoftlimd_for_all,
+		.maxlen		= sizeof(cgroup_memory_ksoftlimd_for_all),
+		.mode		= 0644,
+		.proc_handler	= mem_cgroup_ksoftlimd_sysctl_handler,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= SYSCTL_ONE,
+	},
+	{
+		.procname	= "cgroup_memory_ksoftlimd_sleep_msec",
+		.data		= &cgroup_memory_ksoftlimd_sleep_msec,
+		.maxlen		= sizeof(cgroup_memory_ksoftlimd_sleep_msec),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= &one_hundred,
+		.extra2		= &ten_thousand,
+	},
+	{
+		.procname	= "cgroup_memory_ksoftlimd_loops",
+		.data		= &cgroup_memory_ksoftlimd_loops,
+		.maxlen		= sizeof(cgroup_memory_ksoftlimd_loops),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= SYSCTL_ONE,
+		.extra2		= &one_thousand,
 	},
 #endif
 	{ }
