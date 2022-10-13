85d719409c067ad6ce69d4fd219dc1924e68e44a
Pavel Snajdr <snajpa@snajpa.net>
2022-10-13 17:43:40 +0200

    [vpsAdminOS] memcg: skip shrink_slab when cgroup_memory_nokmem
    Signed-off-by: Pavel Snajdr <snajpa@snajpa.net>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -88,6 +88,11 @@ static bool cgroup_memory_nosocket __ro_after_init;
 /* Kernel memory accounting disabled? */
 static bool cgroup_memory_nokmem __ro_after_init;
 
+bool cgroup_memory_kmem_enabled(void)
+{
+	return !cgroup_memory_nokmem;
+}
+
 #ifdef CONFIG_CGROUP_WRITEBACK
 static DECLARE_WAIT_QUEUE_HEAD(memcg_cgwb_frn_waitq);
 #endif
