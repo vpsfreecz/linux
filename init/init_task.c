bbf365d96bc380d1bb48cd4748f1b13eb98e7b15
Pavel Snajdr <snajpa@snajpa.net>
2022-11-04 10:01:12 +0100

    [vpsAdminOS] limit visible cpus for containers v4.5
    Signed-off-by: Pavel Snajdr <snajpa@snajpa.net>

--- init/init_task.c
+++ init/init_task.c
@@ -81,6 +81,8 @@ struct task_struct init_task
 	.policy		= SCHED_NORMAL,
 	.cpus_ptr	= &init_task.cpus_mask,
 	.cpus_mask	= CPU_MASK_ALL,
+	.set_fake_cpu_mask = 0,
+	.fake_cpu_mask = CPU_MASK_ALL,
 	.nr_cpus_allowed= NR_CPUS,
 	.mm		= NULL,
 	.active_mm	= &init_mm,
