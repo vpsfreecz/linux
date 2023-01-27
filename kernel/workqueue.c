bafb843ebeb14cd2538a8d939abea6679f4f25f3
Pavel Snajdr <snajpa@snajpa.net>
2023-01-27 10:44:35 +0100

    [vpsAdminOS] dump_stack: print memcg path v2

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -53,6 +53,7 @@
 #include <linux/nmi.h>
 #include <linux/kvm_para.h>
 #include <linux/delay.h>
+#include <linux/memcontrol.h>
 
 #include "workqueue_internal.h"
 
@@ -5036,6 +5037,8 @@ void print_worker_info(const char *log_lvl, struct task_struct *task)
 	struct pool_workqueue *pwq = NULL;
 	struct workqueue_struct *wq = NULL;
 	struct worker *worker;
+
+	mem_cgroup_print_dump_stack_context(log_lvl, task);
 
 	if (!(task->flags & PF_WQ_WORKER))
 		return;
