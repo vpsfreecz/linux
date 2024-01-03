3e10a2ec5c3eed4a6999c89cd00cfd5e586a9890
Pavel Snajdr <snajpa@snajpa.net>
2024-01-04 00:35:09 +0100

    [vpsAdminOS] Virtualize loadavg v4
    Use 1st level cgroup namespace cpu cgroup to virtualize loadavg in vpsAdminOS
    
    This patch makes cgroup ns hierarchical, it is so far the only user.
    
    Also this patch introduces /proc/vpsadminos.
    
    Signed-off-by: Pavel Snajdr <snajpa@snajpa.net>

--- include/linux/vpsadminos.h
+++ include/linux/vpsadminos.h
@@ -24,8 +24,13 @@ extern void fake_sysctl_bufs_free(struct user_namespace *ns);
 
 extern unsigned int online_cpus_in_cpu_cgroup(struct task_struct *p);
 void fake_cputime_readout(struct task_struct *p, u64 timestamp, u64 *user, u64 *system, int *cpus);
+u64 fake_cputime_readout_idle(struct timespec64 *uptime, struct task_struct *p);
 void fake_cputime_readout_percpu(struct task_struct *p, int cpu, u64 *user, u64 *system);
 extern void set_fake_affinity_cpumask(struct task_struct *p, const struct cpumask *srcmask);
 extern int fake_affinity_cpumask(struct task_struct *p, struct cpumask *dstmask);
 extern int fake_online_cpumask(struct task_struct *p, struct cpumask *dstmask);
+
+struct proc_dir_entry;
+extern struct proc_dir_entry *proc_vpsadminos;
+extern int virt_loadavg_proc_show(struct seq_file *m, void *v);
 #endif
