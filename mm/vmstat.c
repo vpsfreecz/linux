b326f1f58f2c353980d97e12678ae401df9bf86d
Pavel Snajdr <snajpa@snajpa.net>
2024-04-28 13:35:14 +0200

    [vpsAdminOS] /proc/stat: virtualize oom_kill v2
    Signed-off-by: Pavel Snajdr <snajpa@snajpa.net>

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -28,6 +28,7 @@
 #include <linux/mm_inline.h>
 #include <linux/page_owner.h>
 #include <linux/sched/isolation.h>
+#include <linux/vpsadminos.h>
 
 #include "internal.h"
 
@@ -1846,26 +1847,19 @@ static int vmstat_show(struct seq_file *m, void *arg)
 	unsigned long *l = arg;
 	unsigned long off = l - (unsigned long *)m->private;
 	unsigned long fake = 0;
+	struct mem_cgroup *mem;
 
-	do {
-		struct mem_cgroup *mem, *mem_orig, *cgns_root_mem;
-		if (strncmp(vmstat_text[off], "oom_kill", 8))
-			break;
-		mem_orig = mem = get_mem_cgroup_from_mm(current->mm);
-		if (!mem)
-			break;
-		if (mem_cgroup_is_root(mem))
-			break;
-		cgns_root_mem = mem_cgroup_from_css(current->nsproxy->cgroup_ns->root_cset->subsys[memory_cgrp_id]);
-		if (mem_cgroup_is_root(cgns_root_mem))
-			break;
-		while (parent_mem_cgroup(mem) != cgns_root_mem)
-			mem = parent_mem_cgroup(mem);
-		WARN_ON_ONCE(mem_cgroup_is_root(mem));
-		fake = atomic_long_read(&mem->memory_events[MEMCG_OOM_KILL]);
-		l = &fake;
-		css_put(&mem_orig->css);
-	} while (0);
+	if (strncmp(vmstat_text[off], "oom_kill", 8))
+		goto oom_orig;
+
+	mem = get_current_most_limited_memcg();
+	if (!mem)
+		goto oom_orig;
+
+	fake = atomic_long_read(&mem->memory_events[MEMCG_OOM_KILL]);
+	l = &fake;
+	mem_cgroup_put(mem);
+oom_orig:
 
 	seq_puts(m, vmstat_text[off]);
 	seq_put_decimal_ull(m, " ", *l);
