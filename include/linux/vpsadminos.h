a9012b19a0b4062e418ae9d3be2205b0a19b368a
Pavel Snajdr <snajpa@snajpa.net>
2024-05-06 21:36:20 +0200

    [vpsAdminOS] Virtualize module parameters
    ... just fake them
    
    Signed-off-by: Pavel Snajdr <snajpa@snajpa.net>

--- include/linux/vpsadminos.h
+++ include/linux/vpsadminos.h
@@ -1,3 +1,11 @@
+#ifndef VPSADMINOS_H
+#define VPSADMINOS_H
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/mm.h>
+#include <linux/vmstat.h>
+#include <linux/atomic.h>
 #include <linux/memcontrol.h>
 #include <linux/user_namespace.h>
 
@@ -15,3 +23,10 @@ static inline struct user_namespace *current_1stlvl_user_ns(void)
 }
 
 struct mem_cgroup *get_current_most_limited_memcg(void);
+
+extern ssize_t fake_sysfs_kf_read(struct kernfs_open_file *of, char *buf);
+extern ssize_t fake_sysfs_kf_write(struct kernfs_open_file *of, char *buf,
+				   size_t count, loff_t pos);
+extern void fake_sysctl_bufs_init(struct user_namespace *ns);
+extern void fake_sysctl_bufs_free(struct user_namespace *ns);
+#endif
