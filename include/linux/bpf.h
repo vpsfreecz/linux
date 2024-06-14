447026caac5dcc50e1f3146ce8c295d6cd0aecbf
Pavel Snajdr <snajpa@snajpa.net>
2024-06-14 23:24:46 +0200

    [vpsAdminOS] unprivileged BPF: round down time
    Signed-off-by: Pavel Snajdr <snajpa@snajpa.net>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -1851,6 +1851,7 @@ static inline void bpf_map_dec_elem_count(struct bpf_map *map)
 }
 
 extern int sysctl_unprivileged_bpf_disabled;
+extern int sysctl_unprivileged_bpf_time_adjust_nsec;
 
 static inline bool bpf_allow_ptr_leaks(void)
 {
