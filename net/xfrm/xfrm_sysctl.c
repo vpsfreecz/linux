a2a4a3686b188d6771e7da1117bd704177c74675
Pavel Snajdr <snajpa@snajpa.net>
2024-08-17 21:11:36 +0200

    [vpsAdminOS] enable ipsec sysctls in 1stlvl userns

--- net/xfrm/xfrm_sysctl.c
+++ net/xfrm/xfrm_sysctl.c
@@ -56,7 +56,7 @@ int __net_init xfrm_sysctl_init(struct net *net)
 	table[3].data = &net->xfrm.sysctl_acq_expires;
 
 	/* Don't export sysctls to unprivileged users */
-	if (net->user_ns != &init_user_ns)
+	if (net->user_ns != &init_user_ns && net->user_ns->parent != &init_user_ns)
 		table_size = 0;
 
 	net->xfrm.sysctl_hdr = register_net_sysctl_sz(net, "net/core", table,
