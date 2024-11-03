061adefc128e21501d9511b4e8d22ec9394742ff
Pavel Snajdr <snajpa@snajpa.net>
2024-11-03 17:22:07 +0100

    [vpsAdminOS] limit visible cpus for containers v16
    Signed-off-by: Pavel Snajdr <snajpa@snajpa.net>

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -41,6 +41,7 @@
 #include <linux/sched/isolation.h>
 #include <linux/wait.h>
 #include <linux/workqueue.h>
+#include <linux/vpsadminos.h>
 
 DEFINE_STATIC_KEY_FALSE(cpusets_pre_enable_key);
 DEFINE_STATIC_KEY_FALSE(cpusets_enabled_key);
@@ -3109,6 +3110,10 @@ ssize_t cpuset_write_resmask(struct kernfs_open_file *of,
 	struct cpuset *trialcs;
 	int retval = -ENODEV;
 
+	if ((current->nsproxy && current->nsproxy->cgroup_ns != &init_cgroup_ns) &&
+	    (online_cpus_in_cpu_cgroup(current) > 0))
+		return nbytes;
+
 	buf = strstrip(buf);
 	cpus_read_lock();
 	mutex_lock(&cpuset_mutex);
@@ -3157,7 +3162,26 @@ int cpuset_common_seq_show(struct seq_file *sf, void *v)
 	struct cpuset *cs = css_cs(seq_css(sf));
 	cpuset_filetype_t type = seq_cft(sf)->private;
 	int ret = 0;
+	cpumask_t fake_mask;
 
+	if (current->nsproxy && current->nsproxy->cgroup_ns != &init_cgroup_ns) {
+		if (!fake_online_cpumask(current, &fake_mask))
+			goto orig;
+		switch (type) {
+		case FILE_CPULIST:
+		case FILE_EFFECTIVE_CPULIST:
+			seq_printf(sf, "%*pbl\n", cpumask_pr_args(&fake_mask));
+			break;
+		case FILE_MEMLIST:
+		case FILE_EFFECTIVE_MEMLIST:
+			seq_printf(sf, "0\n");
+			break;
+		default:
+		}
+		return ret;
+	}
+
+orig:
 	spin_lock_irq(&callback_lock);
 
 	switch (type) {
