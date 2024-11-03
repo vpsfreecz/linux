4d1a5c6dc61dba16d9cb58ed97e359d429e3db27
Pavel Snajdr <snajpa@snajpa.net>
2024-11-03 17:22:07 +0100

    [vpsAdminOS] limit visible cpus for containers v19
    Signed-off-by: Pavel Snajdr <snajpa@snajpa.net>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -64,6 +64,8 @@
 #include <linux/vtime.h>
 #include <linux/wait_api.h>
 #include <linux/workqueue_api.h>
+#include <linux/user_namespace.h>
+#include <linux/vpsadminos.h>
 
 #ifdef CONFIG_PREEMPT_DYNAMIC
 # ifdef CONFIG_GENERIC_ENTRY
@@ -8230,6 +8232,25 @@ long sched_setaffinity(pid_t pid, const struct cpumask *in_mask)
 		goto out_put_task;
 	}
 
+	if (current->nsproxy && current->nsproxy->cgroup_ns != &init_cgroup_ns) {
+		struct cpumask fake_mask;
+		if (!fake_online_cpumask(p, &fake_mask))
+			goto orig;
+		if (!check_same_owner(p)) {
+		rcu_read_lock();
+			if (!ns_capable(__task_cred(p)->user_ns, CAP_SYS_NICE)) {
+				rcu_read_unlock();
+				return -EPERM;
+		}
+		rcu_read_unlock();
+		}
+		if (!cpumask_subset(in_mask, &fake_mask))
+			return -EINVAL;
+		set_fake_affinity_cpumask(p, in_mask);
+		return 0;
+	}
+orig:
+
 	if (!check_same_owner(p)) {
 		rcu_read_lock();
 		if (!ns_capable(__task_cred(p)->user_ns, CAP_SYS_NICE)) {
@@ -10759,7 +10780,7 @@ static long tg_get_cfs_burst(struct task_group *tg)
 	return burst_us;
 }
 
-static s64 cpu_cfs_quota_read_s64(struct cgroup_subsys_state *css,
+s64 cpu_cfs_quota_read_s64(struct cgroup_subsys_state *css,
 				  struct cftype *cft)
 {
 	return tg_get_cfs_quota(css_tg(css));
@@ -10771,7 +10792,7 @@ static int cpu_cfs_quota_write_s64(struct cgroup_subsys_state *css,
 	return tg_set_cfs_quota(css_tg(css), cfs_quota_us);
 }
 
-static u64 cpu_cfs_period_read_u64(struct cgroup_subsys_state *css,
+u64 cpu_cfs_period_read_u64(struct cgroup_subsys_state *css,
 				   struct cftype *cft)
 {
 	return tg_get_cfs_period(css_tg(css));
