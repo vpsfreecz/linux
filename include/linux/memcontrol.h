8c96933dd74a054b33e5b7158a97ad52f07dffa2
Pavel Snajdr <snajpa@snajpa.net>
2025-04-16 01:11:04 +0200

    [vpsAdminOS] Virtualize meminfo and swaps v4
    Signed-off-by: Pavel Snajdr <snajpa@snajpa.net>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -1508,6 +1508,11 @@ static inline unsigned long memcg_page_state(struct mem_cgroup *memcg, int idx)
 	return 0;
 }
 
+static inline unsigned long memcg_page_state_nowarn(struct mem_cgroup *memcg, int idx)
+{
+	return 0;
+}
+
 static inline unsigned long lruvec_page_state(struct lruvec *lruvec,
 					      enum node_stat_item idx)
 {
